7ea6c0678491d4e18eb1be2f3629aa1f618574f0
Aituar Abdiluly <aituufs@gmail.com>
2018-11-04 15:37:36 +0100

    Initial Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart.loc/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart.loc/admin/');
+define('DIR_SYSTEM', '/var/www/html/opencart.loc/system/');
+define('DIR_IMAGE', '/var/www/html/opencart.loc/image/');
+define('DIR_LANGUAGE', '/var/www/html/opencart.loc/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart.loc/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/opencart.loc/system/config/');
+define('DIR_CACHE', '/var/www/html/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart.loc/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/opencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
